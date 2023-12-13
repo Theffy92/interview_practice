eb54fecd2a76034d9d18db465301f02230af49b0
Elena Estefania Reyes <estefyreyes.92@gmail.com>
2023-12-12 21:58:34 -0500

    solve case sensitive palindrome

--- Palindrome/palindrome_str.py
+++ Palindrome/palindrome_str.py
@@ -7,20 +7,39 @@
 #3- reverse the word and store it in a new variable
 #4- compare the reverse string with the actual string
 
+# def palindrome(string):
+#     if type(string) != str:
+#         raise TypeError
+#     print(string)
+#     new_string = ""
+#     for char in string:
+#         if char != " ":
+#             new_string += char
+            
+#     string = "".join(new_string)
+    
+#     rev_string = new_string[::-1]
+#     print("string now is: " + string)
+#     print("reverse is:" + rev_string)
+#     return rev_string == string
+
+# print(palindrome("nurses run"))
+
 def palindrome(string):
     if type(string) != str:
         raise TypeError
-    print(string)
-    new_string = ""
-    for char in string:
-        if char != " ":
-            new_string += char
-            
-    string = "".join(new_string)
     
-    rev_string = new_string[::-1]
-    print("string now is: " + string)
-    print("reverse is:" + rev_string)
-    return rev_string == string
+    string_lower= string.lower()
+    word_no_blanks = ""
+    for char in string_lower:
+        if char != " ":
+            word_no_blanks += char
+            
+    string_lower = "".join(word_no_blanks)
+    print(string_lower)
+    rev_string = string_lower[::-1]
+    
+    return rev_string == string_lower
 
-print(palindrome("nurses run"))
+print(palindrome("Race cAr"))
+print(palindrome("mOm"))
