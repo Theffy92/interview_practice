25ae5718be5b2505773fb91401cebf91e63b23cd
Elena Estefania Reyes <estefyreyes.92@gmail.com>
2023-12-12 22:53:40 -0500

    solve palindrome space complexity

--- Palindrome/palindrome_str.py
+++ Palindrome/palindrome_str.py
@@ -25,21 +25,54 @@
 
 # print(palindrome("nurses run"))
 
-def palindrome(string):
-    if type(string) != str:
+#case sensitive
+
+# def palindrome(string):
+#     if type(string) != str:
+#         raise TypeError
+    
+#     string_lower= string.lower()
+#     word_no_blanks = ""
+#     for char in string_lower:
+#         if char != " ":
+#             word_no_blanks += char
+            
+#     string_lower = "".join(word_no_blanks)
+#     print(string_lower)
+#     rev_string = string_lower[::-1]
+    
+#     return rev_string == string_lower
+
+# O(1) Space complexity
+def palindrome(word):
+    if type(word) != str:
         raise TypeError
     
-    string_lower= string.lower()
-    word_no_blanks = ""
-    for char in string_lower:
-        if char != " ":
-            word_no_blanks += char
-            
-    string_lower = "".join(word_no_blanks)
-    print(string_lower)
-    rev_string = string_lower[::-1]
+    #lower cased the entire string
+    word_lower = word.lower()
     
-    return rev_string == string_lower
+    #initialize pointers for the start and end of the string
+    start, end = 0, len(word_lower)-1
+    
+    while start < end:
+        #remove every whitespace before start
+        while start < end and word_lower[start] == " ":
+            start += 1
+        
+        #remove every whitespace before end
+        while start < end and word_lower[end] == " ":
+            end -= 1
+        print("the start is: " + word_lower[start])
+        print("the end is: " + word_lower[end])
+        
+        if word_lower[start] != word_lower[end]:
+            return False
+        
+        start += 1
+        end -= 1
+    
+    return True
 
 print(palindrome("Race cAr"))
+print(palindrome("pronunciation"))
 print(palindrome("mOm"))
