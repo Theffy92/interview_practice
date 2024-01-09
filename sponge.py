6ffa6a5e30cdb2b7088bd08182dafca6ae986045
Elena Estefania Reyes <estefyreyes.92@gmail.com>
2024-01-08 22:07:24 -0500

    add test cases

--- sponge.py
+++ sponge.py
@@ -13,3 +13,14 @@ sponge_case("E") # Should return "e"
 
 sponge_case("e") # Should return "e"
 """
+
+def sponge_case(sentence):
+    # Write your solution here!
+    pass
+
+# Test cases
+assert sponge_case("spongebob") == "sPoNgEbOb"
+assert sponge_case("Who are YOU calling A Pinhead") == "wHo aRe yOu cAlLiNg a pInHeAd"
+assert sponge_case("WHAT is UP my dude") == "wHaT iS uP mY dUdE"
+assert sponge_case("E") == "e"
+assert sponge_case("e") == "e"
