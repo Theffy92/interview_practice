bc808f0afbe3f2670154794e4c471b32ac961a86
Elena Estefania Reyes <estefyreyes.92@gmail.com>
2024-01-08 21:56:28 -0500

    add integer reverse instead of string

--- Palindrome/new_palindrome_practice.py
+++ Palindrome/new_palindrome_practice.py
@@ -11,19 +11,22 @@ def palindrome(num):
     
     #the length of digits in the number
     len_num = int(math.log10(abs(num))) + 1
-    num_rev = ' '
+    # num_rev = ' '
+    num_rev = 0
     modulo = 0
     count = 0
     num_copy = num
     
     while count < len_num:
         modulo = num_copy % 10
-        num_rev += str(modulo) 
+        num_rev = num_rev * 10 + modulo
+        # num_rev += str(modulo) 
         num_copy = int(num_copy / 10)
         count +=1
         
-    return int(num_rev) == num
+    return num_rev == num
 
 print(palindrome(121))
 print(palindrome(-121))
+print(palindrome(1))
 #print(palindrome(-525))
