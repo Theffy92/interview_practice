c1eeb397971ab575ff8e690ba4e49f8add4906fa
Elena Estefania Reyes <estefyreyes.92@gmail.com>
2024-01-08 23:14:56 -0500

    add racers solution

--- racers.py
+++ racers.py
@@ -1,7 +1,8 @@
 """
 Your goal is to write a function to identify which drivers in a dataset have NOT won any race.
 
-The dataset will be a dictionary. The keys will be the names of races and the values will be, the top three drivers to finish at that race. The first driver in the tuple will be the one who won the race. An example dataset is shown below:
+The dataset will be a dictionary. The keys will be the names of races and the values will be, the top three drivers to finish at that race. 
+The first driver in the tuple will be the one who won the race. An example dataset is shown below:
 
 races_1 = {
     "Suzuka": ("Tsunoda", "Latifi", "Stroll"),
@@ -9,9 +10,11 @@ races_1 = {
     "Silverstone": ("Hamilton", "Latifi", "Tsunoda")
 }
 
-In this example, Tsunoda won at Suzuka, Pérez won at Mexico City, and Hamilton won at Silverstone. Latifi and Stroll are present in the dictionary, but they did not win any of the races.
+In this example, Tsunoda won at Suzuka, Pérez won at Mexico City, and Hamilton won at Silverstone. Latifi and Stroll are present in the dictionary, but they did 
+not win any of the races.
 
-Write a function that accepts a races dicitonary and returns a *set* of all drivers that are present in that dictionary but did NOT win any race. For example, for the above dictionary, your function should return:
+Write a function that accepts a races dicitonary and returns a *set* of all drivers that are present in that dictionary but did NOT win any race. For example, 
+for the above dictionary, your function should return:
 
 {"Latifi", "Stroll"}
 
@@ -21,7 +24,24 @@ looping through the tuple and save in a new list all the items but the first one
 """
 def non_winners(races):
     # Your solution here!
-    pass
+    no_winner_list = []
+    winner_list = []
+    for drivers_tuple in races.values():
+        for i in range(len(drivers_tuple)):
+            if i > 0:
+                no_winner_list.append(drivers_tuple[i])
+            else:
+                winner_list.append(drivers_tuple[i])
+
+    no_winners_set = set()
+
+    for driver in no_winner_list:
+        if driver not in winner_list:
+            no_winners_set.add(driver)
+    print(no_winners_set)
+    
+    return no_winners_set
+
 
 races_1 = {
     "Suzuka": ("Tsunoda", "Latifi", "Stroll"),
