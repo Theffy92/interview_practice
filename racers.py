5d1faacb1c50130019bfe8c460ee17e6e5782067
Elena Estefania Reyes <estefyreyes.92@gmail.com>
2024-01-08 22:05:27 -0500

    add new racer file

---
+++ racers.py
@@ -0,0 +1,45 @@
+"""
+Your goal is to write a function to identify which drivers in a dataset have NOT won any race.
+
+The dataset will be a dictionary. The keys will be the names of races and the values will be, the top three drivers to finish at that race. The first driver in the tuple will be the one who won the race. An example dataset is shown below:
+
+races_1 = {
+    "Suzuka": ("Tsunoda", "Latifi", "Stroll"),
+    "Mexico City": ("Pérez", "Hamilton", "Tsunoda"),
+    "Silverstone": ("Hamilton", "Latifi", "Tsunoda")
+}
+
+In this example, Tsunoda won at Suzuka, Pérez won at Mexico City, and Hamilton won at Silverstone. Latifi and Stroll are present in the dictionary, but they did not win any of the races.
+
+Write a function that accepts a races dicitonary and returns a *set* of all drivers that are present in that dictionary but did NOT win any race. For example, for the above dictionary, your function should return:
+
+{"Latifi", "Stroll"}
+
+See assertions below for more test cases.
+
+looping through the tuple and save in a new list all the items but the first one
+"""
+def non_winners(races):
+    # Your solution here!
+    pass
+
+races_1 = {
+    "Suzuka": ("Tsunoda", "Latifi", "Stroll"),
+    "Mexico City": ("Pérez", "Hamilton", "Tsunoda"),
+    "Silverstone": ("Hamilton", "Latifi", "Tsunoda")
+}
+assert non_winners(races_1) == {"Latifi", "Stroll"}
+
+races_2 = {
+    "Mexico City": ("Pérez", "Hamilton", "Tsunoda"),
+}
+assert non_winners(races_2) == {"Hamilton", "Tsunoda"}
+
+races_3 = {
+    "Monaco": ("Leclerc", "Verstappen", "Sainz"),
+    "Barcelona": ("Sainz", "Verstappen", "Leclerc"),
+    "Zandvoort": ("Verstappen", "Sainz", "Leclerc")
+}
+# If all drivers present in the dictionary won a race
+# then the return value should be an empty set
+assert non_winners(races_3) == set()
