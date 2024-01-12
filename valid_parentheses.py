ec27e8303d401674f3433ffa5b1d5b1da91d9576
Elena Estefania Reyes <estefyreyes.92@gmail.com>
2024-01-12 01:43:26 -0500

    add another parentheses solution

--- valid_parentheses.py
+++ valid_parentheses.py
@@ -25,3 +25,37 @@ Constraints:
 
 1 <= s.length <= 104
 s consists of parentheses only '()[]{}'.'''
+
+#I will create a list variable to use it as a stack 
+# and work with the LIFO theory to check if every open ang closing parentheses matches
+# For this I will also create a dictionary that store the closing parentheses as keys and 
+# the open parentheses
+
+def valid_parentheses(s):
+    stack= []
+    #brackets_map = {')':'(', '}':'{', ']':'['}
+    
+    # for char in s:
+    #     if char in bracket_map.values():
+    #         stack.append(char)
+    #     elif char in bracket_map.keys():
+    #         if not stack or stack.pop() != bracket_map[char]:
+    #             return False
+    #     else:
+    #         return False
+    brackets_map = {'(':')', '{':'}', '[':']'}
+    
+    for char in s:
+        if char in brackets_map.keys():
+            stack.append(char)
+        elif char in brackets_map.values():
+            if not stack or stack.pop() != list(brackets_map.keys())[list(brackets_map.values()).index(char)]:
+                return False
+        else:
+            return False
+
+    return not stack
+
+print(valid_parentheses('()[]')) #True
+print(valid_parentheses('()[')) #False
+print(valid_parentheses(']{}[]')) #False
