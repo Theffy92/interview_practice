67deafa6f9d151d4a2a737e9fc13eaf0d9cf433d
Elena Estefania Reyes <estefyreyes.92@gmail.com>
2024-01-11 21:08:04 -0500

    add propmt

---
+++ mode.py
@@ -0,0 +1,6 @@
+'''
+Given a list of numbers, find the mode of the set. 
+Ex: given: [7,3,5,8,3,6,1,3,4,5], mode: 3
+The number 3 occurs three times in this set of integers. This is more than any other integer, so the mode is 3. 
+- Asked about edge cases after
+'''
