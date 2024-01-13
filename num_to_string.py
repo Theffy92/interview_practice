02f347b207877da15a9dd806f0a1563b7d4b15a3
Elena Estefania Reyes <estefyreyes.92@gmail.com>
2024-01-13 00:40:11 -0500

    add simple solution

--- num_to_string.py
+++ num_to_string.py
@@ -1,3 +1,13 @@
 '''
 Num to text; i.e. input = 3 output = "three"
 '''
+
+def num_to_text(num):
+    num_map= {0:'zero', 1:'one', 2:'two', 3:'three', 4:'four', 5:'five'}
+    
+    for n, text in num_map.items():
+        if num == n:
+            return text
+        
+        
+print(num_to_text(3))
