824b255d9fa65ff4dc114ceb3ea19df0af69de7e
Elena Estefania Reyes <estefyreyes.92@gmail.com>
2024-01-14 16:04:49 -0500

    palindrome with floor division

--- Palindrome/new_palindrome_practice.py
+++ Palindrome/new_palindrome_practice.py
@@ -2,32 +2,46 @@
 
 #import math
 
-def palindrome(num):
-    if not isinstance(num, int):
-        raise TypeError('Invalid input type')
+# def palindrome(num):
+#     if not isinstance(num, int):
+#         raise TypeError('Invalid input type')
     
-    if num == 0:
-        return True
+#     if num == 0:
+#         return True
     
-    #the length of digits in the number
-    len_num = len(str(abs(num)))
-    # len_num = int(math.log10(abs(num))) + 1
-    # num_rev = ' '
-    num_rev = 0
-    modulo = 0
-    count = 0
-    num_copy = num
+#     #the length of digits in the number
+#     len_num = len(str(abs(num)))
+#     # len_num = int(math.log10(abs(num))) + 1
+#     # num_rev = ' '
+#     num_rev = 0
+#     modulo = 0
+#     count = 0
+#     num_copy = num
     
-    while count < len_num:
-        modulo = num_copy % 10
-        num_rev = num_rev * 10 + modulo
-        # num_rev += str(modulo) 
-        num_copy = int(num_copy / 10)
-        count +=1
+#     while count < len_num:
+#         modulo = num_copy % 10
+#         num_rev = num_rev * 10 + modulo
+#         # num_rev += str(modulo) 
+#         num_copy = int(num_copy / 10)
+#         count +=1
         
-    return num_rev == num
+#     return num_rev == num
 
-print(palindrome(121))
-print(palindrome(-121))
-print(palindrome(1))
+def is_palindrome(num):
+    len_num = len(str(abs(num)))
+    mod = 0
+    rev = 0
+    num_alt = num
+    
+    while len_num >= 1:
+        mod = num_alt % 10
+        rev = rev * 10 + mod
+        num_alt = num_alt // 10
+        len_num -= 1
+    
+    return rev == num
+
+print(is_palindrome(121))
+print(is_palindrome(-121))
+print(is_palindrome(1))
 #print(palindrome(-525))
