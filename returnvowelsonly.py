073f5a554df570dd115bbca78338279bbefd65fd
Elena Estefania Reyes <estefyreyes.92@gmail.com>
2024-01-14 19:39:40 -0500

    solve vowels only problem

--- returnvowelsonly.py
+++ returnvowelsonly.py
@@ -1,1 +1,17 @@
 #Given a string return vowels only
+
+def vowels_only(s):
+    vowels_set = set('aeiouAEIOU')
+    vowels_list = []
+    index = 0
+    while index <= len(s)-1:
+        if s[index] in vowels_set and s[index] not in vowels_list:
+            vowels_list.append(s[index])
+            index += 1
+        else:
+            index += 1
+            
+    return ''.join(vowels_list)
+
+print(vowels_only('hola que tal'))
+print(vowels_only('vowels everywhere'))
