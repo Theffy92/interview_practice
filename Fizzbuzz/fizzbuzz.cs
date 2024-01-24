6c2d69b5d9825ef1b31841267a8688455dd8ceb5
Elena Estefania Reyes <estefyreyes.92@gmail.com>
2024-01-23 20:13:29 -0500

    add new folder

---
+++ Fizzbuzz/fizzbuzz.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static List<string> FizzBuzz(int num)
+    {
+        List<string> arr = new List<string>();
+        for (int i = 1; i <= num; i++)
+        {
+            if (i % 3 == 0 && i % 5 == 0)
+            {
+                arr.Add("FizzBuzz");
+            }
+            else if (i % 5 == 0)
+            {
+                arr.Add("Buzz");
+            }
+            else if (i % 3 == 0)
+            {
+                arr.Add("Fizz");
+            }
+            else
+            {
+                arr.Add(i.ToString());
+            }
+        }
+        return arr;
+    }
+
+    static void Main()
+    {
+        List<string> result = FizzBuzz(15);
+        foreach (var item in result)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
